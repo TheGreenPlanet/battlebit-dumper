defbdb87eee1f68f01e858db9c559720110a46e4
TheGreenPlanet <48806927+TheGreenPlanet@users.noreply.github.com>
2024-02-27 15:46:52 +0100

    Remove unused function

--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -1,15 +1,4 @@
-use std::fmt;
-
 mod misc;
-
-// Nicely format supposedly valid identifier-like strings
-fn ident(s: &str) -> impl '_ + fmt::Display {
-	fmtools::fmt! { move
-		if s.is_empty() { "{empty}" }
-		else if s.contains("\"") || s.contains(" ") || s.contains("\n") { {s:?} }
-		else { {s} }
-	}
-}
 
 pub fn parse(o: &mut String, image: &[u8]) {
 	use pelite::pe64::*;
